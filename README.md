08ee58f86e53a3a5bacebf3ecea121456c987625
HenrickC <106536163+HenrickC@users.noreply.github.com>
2025-01-13 01:39:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-firstrepository
+
 projeto para treinar programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function home(){
-    return <h1>Eu amo você, Se você me ama, dá uma risadinha! :) </h1>
+function home() {
+  return <h1>Eu amo você, Se você me ama, dá uma risadinha! :) </h1>;
 }
 
+function teste1() {
+  console.log("Goiabinha");
+}
 export default home;
